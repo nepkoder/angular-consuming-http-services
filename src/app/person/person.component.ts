f463d6a3cd7a835bc3b6a57fdac2382670117246
Sujan Shrestha <nepkoder@gmail.com>
2018-06-06 16:49:44 +0545

    added delete and update feature services

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -6,17 +6,43 @@ import { Http, Response } from '@angular/http';
   templateUrl: './person.component.html',
   styleUrls: ['./person.component.css']
 })
-export class PersonComponent implements OnInit {
+export class PersonComponent  {
 
   persons: any[];
 
-  constructor(http: Http) {
-    http.get('http://localhost:8080/api/persons').subscribe(response => {
+  private url = 'http://localhost:8080/api/persons';
+
+  constructor(private http: Http) {
+    http.get(this.url).subscribe(response => {
       this.persons = response.json();
     });
    }
 
-  ngOnInit() {
+  createPerson(input: HTMLInputElement) {
+    const p = {name: input.value};
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(p)).subscribe(response => {
+      p['id'] = response.json().id;
+      this.persons.splice(0, 0, p);
+      // console.log(response.json());
+    });
+  }
+
+
+  editPerson(p) {
+    this.http.patch(this.url, JSON.stringify({ isRead: true})).subscribe(response => {
+      console.log(response.json());
+    } );
+  }
+
+  deletePerson(p) {
+    this.http.delete(this.url + '/' + p.id).subscribe(response => {
+      // console.log(response.json());
+      const pos = this.persons.indexOf(p);
+      this.persons.splice(pos, 1);
+
+    });
   }
 
 }
